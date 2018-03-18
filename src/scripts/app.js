b5946c644ca07ea1c7aa169cb07eebc40fbad5fb
Quilt9 <helen@quilt9.com>
2018-03-18 15:42:59 -0400

    Saving files before refreshing line endings

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,3 @@
-
-
-//========================================
 /*
  * Create a list that holds all of your cards
  */
@@ -39,6 +36,3 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-
-//========================================
